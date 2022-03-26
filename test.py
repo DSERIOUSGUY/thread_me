cffd2e0618b5843bb2d3e9e0884d9695af479cf9
utsav <utsavagarwal2014@gmail.com>
2022-03-26 21:05:18 +0000

    adding matrix threading by row

--- test.py
+++ test.py
@@ -1,21 +1,22 @@
-from thread_lib import matrix_threaded_for
+from thread_lib import matrix_threaded_rows_for
+from multiprocessing import Queue
 
 
 def print_test(temp_matrix):
     data = temp_matrix[0]
 
-    if(len(temp_matrix)>3):
+    if(len(temp_matrix)>2):
         args = temp_matrix[1]
         print(args)
 
-    i = temp_matrix[-2]
-    j = temp_matrix[-1]
+    i = temp_matrix[-1]
 
-    print(data[i][j])
+    print(data[i])
 
 
 test = [[1,2],[3,4,5]]
 
 print(test)
 
-matrix_threaded_for(print_test,test,["hello"])
+
+matrix_threaded_rows_for(print_test,test,["hi"])
--- thread_lib.py
+++ thread_lib.py
@@ -1,4 +1,5 @@
 import threading
+from multiprocessing import Process,Queue
 from time import sleep
 
 timeout = 0.05
@@ -27,6 +28,10 @@ def simple_threaded_for(func, args_list):
 
 def matrix_threaded_for(func, matrix, fn_args_list):
 
+    #WARNING: NOT SUITABLE FOR LARGE MATRIX
+    #may spawn large no. of threads, which even though may be simple
+    #make the CPU large amounts of time in context switching
+
     global timeout
 
     #for each iteration, launch a thread
@@ -41,6 +46,30 @@ def matrix_threaded_for(func, matrix, fn_args_list):
             t = threading.Thread(target=func, args=(args_list,))
             t.start()
             threads_array.append(t)
+
+    #wait for each thread to complete
+    for i in range(len(threads_array)):
+        threads_array[i].join()
+        if(threads_array[i].is_alive):
+            sleep(timeout)
+
+    return 0
+
+
+def matrix_threaded_rows_for(func, matrix, fn_args_list):
+
+    global timeout
+
+    # for each iteration, launch a thread
+    threads_array = []
+    reps_y = len(matrix)
+
+    for i in range(reps_y):
+        reps_x = len(matrix[i])
+        args_list = [matrix] + fn_args_list + [i]
+        t = threading.Thread(target=func, args=(args_list,))
+        t.start()
+        threads_array.append(t)
 
     #wait for each thread to complete
     for i in range(len(threads_array)):
